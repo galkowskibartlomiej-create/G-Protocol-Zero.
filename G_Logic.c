e7c2db929ab05cf524a715e939de295da0756fee
galkowskibartlomiej-create <galkowskibartlomiej@gmail.com>
2026-04-02 01:08:40 +0200

    Create G_Logic.c

---
+++ G_Logic.c
@@ -0,0 +1,10 @@
+/* * G-PROTOCOL CORE | Author: Bartłomiej Gałkowski 
+ * Universal Mathematical Sovereignty Gateway.
+ */
+
+#include <complex.h>
+
+double complex G_Execute_Fractal(double complex z, double complex c, double gamma) {
+    // Fundamental G-Logic: Non-linear phase gate
+    return cpow(z, 2) + c * cexp(I * gamma);
+}
